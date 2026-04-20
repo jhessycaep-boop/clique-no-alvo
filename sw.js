8103e2458fd7b24c5e0c33f72b6479aa230dae74
jhessycaep-boop <jhessycaep@gmail.com>
2026-04-20 16:47:29 -0300

    Refactor cache handling in service worker

--- sw.js
+++ sw.js
@@ -29,9 +29,10 @@ self.addEventListener("install", e=>{
 self.addEventListener("activate", e=>{
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k=>{
-        if(k!==CACHE_NAME) return caches.delete(k);
-      }))
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
     )
   );
   self.clients.claim();
@@ -39,6 +40,13 @@ self.addEventListener("activate", e=>{
 
 self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      })
+      .catch(() => caches.match(e.request))
   );
 });
